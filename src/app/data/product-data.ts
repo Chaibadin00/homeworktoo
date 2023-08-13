07a3581e1351ee486ad12a1e4b0a26bf80079e02
Phawat Phuengdecha <pawat.pooh@gmail.com>
2023-08-13 13:17:22 +0700

    add products filter

--- src/app/data/product-data.ts
+++ src/app/data/product-data.ts
@@ -2,63 +2,63 @@ import { ProductModel } from '../models/product-model';
 
 export const products: ProductModel[] = [
     {
-        image: '../../assets/images/Frontal-Cortax/CAMILLO-RAM-MANAGER.png',
+        image: '../../assets/images/FRONTAL-CORTEX/CAMILLO-RAM-MANAGER.png',
         name: 'CAMILLO RAM MANAGER',
         description: 'Description of Product 1',
         price: 19.99,
         category: 'FRONTAL CORTEX'
     },
     {
-        image: '../../assets/images/Frontal-Cortax/EX-DISK.png',
+        image: '../../assets/images/FRONTAL-CORTEX/EX-DISK.png',
         name: 'EX-DISK',
         description: 'Description of Product 2',
         price: 29.99,
         category: 'FRONTAL CORTEX'
     },
     {
-        image: '../../assets/images/Frontal-Cortax/HEAL-ON-KILL.png',
+        image: '../../assets/images/FRONTAL-CORTEX/HEAL-ON-KILL.png',
         name: 'HEAL-ON-KILL',
         description: 'Description of Product 3',
         price: 14.95,
         category: 'FRONTAL CORTEX'
     },
     {
-        image: '../../assets/images/Frontal-Cortax/LIMBIC-SYSTEM-ENHANCEMENT.png',
+        image: '../../assets/images/FRONTAL-CORTEX/LIMBIC-SYSTEM-ENHANCEMENT.png',
         name: 'LIMBIC SYSTEM ENHANCEMENT',
         description: 'Description of Product 4',
         price: 9.99,
         category: 'FRONTAL CORTEX'
     },
     {
-        image: '../../assets/images/Frontal-Cortax/MECHATRONIC-CORE.png',
+        image: '../../assets/images/FRONTAL-CORTEX/MECHATRONIC-CORE.png',
         name: 'MECHATRONIC CORE',
         description: 'Description of Product 5',
         price: 39.99,
         category: 'FRONTAL CORTEX'
     },
     {
-        image: '../../assets/images/Frontal-Cortax/MEMORY-BOOST.png',
+        image: '../../assets/images/FRONTAL-CORTEX/MEMORY-BOOST.png',
         name: 'MEMORY BOOST',
         description: 'Description of Product 1',
         price: 19.99,
         category: 'FRONTAL CORTEX'
     },
     {
-        image: '../../assets/images/Frontal-Cortax/RAM-UPGRADE.png',
+        image: '../../assets/images/FRONTAL-CORTEX/RAM-UPGRADE.png',
         name: 'RAM UPGRADE',
         description: 'Description of Product 2',
         price: 29.99,
         category: 'FRONTAL CORTEX'
     },
     {
-        image: '../../assets/images/Frontal-Cortax/SELF-ICE.png',
+        image: '../../assets/images/FRONTAL-CORTEX/SELF-ICE.png',
         name: 'SELF ICE',
         description: 'Description of Product 3',
         price: 14.95,
         category: 'FRONTAL CORTEX'
     },
     {
-        image: '../../assets/images/Frontal-Cortax/VISUAL-CORTEX-SUPPORT.png',
+        image: '../../assets/images/FRONTAL-CORTEX/VISUAL-CORTEX-SUPPORT.png',
         name: 'VISUAL CORTEX SUPPORT',
         description: 'Description of Product 4',
         price: 9.99,
@@ -69,7 +69,7 @@ export const products: ProductModel[] = [
         name: 'KIROSHI OPTICS',
         description: 'Description of Product 4',
         price: 9.99,
-        category: ' OCULAR SYSTEM'
+        category: 'OCULAR SYSTEM'
     },
     {
         image: '../../assets/images/CIRCULATORY-SYSTEM/ADRENALINE-BOOSTER.png',
--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { products } from '../data/product-data';
+import { ProductModel } from '../models/product-model';
 
 @Component({
   selector: 'app-product-page',
   templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.css']
 })
-export class ProductPageComponent {
+export class ProductPageComponent implements OnInit {
+  products: ProductModel[] = [];
+  allProducts: ProductModel[] = products;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      const category = params['category'];
+      if (category) {
+        this.products = this.allProducts.filter(product => product.category === category);
+      } else {
+        this.products = this.allProducts;
+      }
+    });
+  }
 }
