43bba153e90142d629cedf14eee63c8039cf18d3
Phawat Phuengdecha <pawat.pooh@gmail.com>
2023-09-05 16:07:59 +0700

    add user data

---
+++ src/app/data/user-data.ts
@@ -0,0 +1,21 @@
+[
+    {
+      "id": 1,
+      "name": "John Doe",
+      "email": "johndoe@example.com",
+      "password": "password123"
+    },
+    {
+      "id": 2,
+      "name": "Alice Smith",
+      "email": "alice.smith@example.com",
+      "password": "secret@456"
+    },
+    {
+      "id": 3,
+      "name": "Bob Johnson",
+      "email": "bob.johnson@example.com",
+      "password": "myp@ssw0rd"
+    }
+  ]
+  
